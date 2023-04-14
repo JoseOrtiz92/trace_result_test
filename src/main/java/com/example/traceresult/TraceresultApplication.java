cb247c0f628f1075e4ff819e7f42f95d80766378
JoseOrtiz92 <jose.ortiz.cadiz@gmail.com>
2023-04-14 13:11:15 +0200

    structure

---
+++ src/main/java/com/example/traceresult/TraceresultApplication.java
@@ -0,0 +1,13 @@
+package com.example.traceresult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceresultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceresultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/traceresult/TraceresultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traceresult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceresultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
